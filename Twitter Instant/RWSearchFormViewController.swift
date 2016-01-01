456b9fcc4e0273c79c43fa83e2a4b81620c22a7a
jechol <ryanrhie9@gmail.com>
2016-01-01 16:54:19 +0900

    Requesting Access to Twitter

--- Twitter Instant/RWSearchFormViewController.swift
+++ Twitter Instant/RWSearchFormViewController.swift
@@ -8,12 +8,22 @@
 
 import UIKit
 import ReactiveCocoa
+import Social
+import Accounts
+
+enum RWTwitterInstantError: Int {
+  case AccessDenied = 0, NoTwitterAccounts, InvalidResponse
+}
+
+let RWTwitterInstantDomain = "TwitterInstant"
 
 class RWSearchFormViewController: UIViewController {
 
   @IBOutlet var searchText: UITextField!
 
   var resultsViewController: RWSearchResultsViewController!
+  var accountStore: ACAccountStore!
+  var twitterAccountType: ACAccountType!
 
   func isValidText(text: String) -> Bool {
     return text.characters.count > 2
@@ -29,6 +39,36 @@ class RWSearchFormViewController: UIViewController {
       .startWithNext { [weak self] in
         self?.searchText.backgroundColor = $0
     }
+
+    self.accountStore = ACAccountStore()
+    self.twitterAccountType = self.accountStore.accountTypeWithAccountTypeIdentifier(ACAccountTypeIdentifierTwitter)
+
+    self.requestAccessToTwitterSignal().startWithSignal { (signal, _) -> () in
+      signal.observeNext { (_) -> () in
+        NSLog("Access granted")
+      }
+      signal.observeFailed { (error) -> () in
+        NSLog("An error occurred: \(error)")
+      }
+    }
+  }
+
+  func requestAccessToTwitterSignal() -> SignalProducer<AnyObject?, NSError> {
+    // 1 - define an error
+    let accessError = NSError(domain: RWTwitterInstantDomain, code: RWTwitterInstantError.AccessDenied.rawValue, userInfo: nil)
+
+    // 2 - create the signal
+    return SignalProducer<AnyObject?, NSError> { [weak self] o, _ in
+      self?.accountStore.requestAccessToAccountsWithType(
+        self?.twitterAccountType, options: nil, completion: {
+          (granted: Bool, error: NSError!) -> Void in
+          if (!granted) {
+            o.sendFailed(accessError)
+          } else {
+            o.sendNext(nil)
+            o.sendCompleted()
+          }
+      })
+    }
   }
 }
-
