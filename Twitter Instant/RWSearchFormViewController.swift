108ea5d8f3f7b61308c0e114a4211c4cb5258190
jechol <ryanrhie9@gmail.com>
2016-01-01 16:25:55 +0900

    Validating the Search Text

--- Twitter Instant/RWSearchFormViewController.swift
+++ Twitter Instant/RWSearchFormViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import ReactiveCocoa
 
 class RWSearchFormViewController: UIViewController {
 
@@ -14,10 +15,18 @@ class RWSearchFormViewController: UIViewController {
 
   var resultsViewController: RWSearchResultsViewController!
 
+  func isValidText(text: String) -> Bool {
+    return text.characters.count > 2
+  }
+
   override func viewDidLoad() {
     super.viewDidLoad()
 
     self.resultsViewController = self.splitViewController!.viewControllers[1] as! RWSearchResultsViewController
+
+    self.searchText.rac_textSignal().toSignalProducer()
+      .map { self.isValidText($0 as! String) ? UIColor.whiteColor() : UIColor.yellowColor() }
+      .startWithNext { self.searchText.backgroundColor = $0 }
   }
 }
 
