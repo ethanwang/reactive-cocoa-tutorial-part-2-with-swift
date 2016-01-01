879841ee13c89f4ab973a98a89e86fb080da3e26
jechol <ryanrhie9@gmail.com>
2016-01-01 23:12:53 +0900

    Searching Twitter

--- Twitter Instant/RWSearchFormViewController.swift
+++ Twitter Instant/RWSearchFormViewController.swift
@@ -47,8 +47,11 @@ class RWSearchFormViewController: UIViewController {
       .then(searchText.rac_textSignal().toSignalProducer())
       .map { $0 as! String }
       .filter { self.isValidText($0) }
+      .flatMap(.Concat, transform: { self.signalForSearchWithText($0) })
       .startWithSignal { (signal, _) -> () in
-        signal.observeNext { NSLog("next: \($0)") }
+        signal.observeNext {
+          NSLog("count: \($0["statuses"]!.count)")
+        }
         signal.observeFailed { NSLog("An error occurred: \($0)") }
     }
   }
@@ -69,6 +72,52 @@ class RWSearchFormViewController: UIViewController {
             o.sendCompleted()
           }
       })
+    }
+  }
+
+  func requestForTwitterSearchWithText(text: String) -> SLRequest {
+    let url = NSURL(string: "https://api.twitter.com/1.1/search/tweets.json")
+    let params = ["q": text]
+
+    let request = SLRequest(forServiceType: SLServiceTypeTwitter, requestMethod: .GET
+      , URL: url, parameters: params)
+    return request
+  }
+
+  func signalForSearchWithText(text: String) -> SignalProducer<NSDictionary, NSError> {
+    // 1 - define the errors
+    let noAccountsError = NSError(domain: RWTwitterInstantDomain, code: RWTwitterInstantError.NoTwitterAccounts.rawValue, userInfo: nil)
+    let invalidResponseError = NSError(domain: RWTwitterInstantDomain, code: RWTwitterInstantError.InvalidResponse.rawValue, userInfo: nil)
+
+    // 2 - create the signal block
+    return SignalProducer<NSDictionary, NSError> { (o, _) in
+      // 3 - create the request
+      let request = self.requestForTwitterSearchWithText(text)
+
+      // 4 - supply a twitter account
+      let twitterAccounts = self.accountStore.accountsWithAccountType(self.twitterAccountType)
+
+      if (twitterAccounts.count == 0) {
+        o.sendFailed(noAccountsError)
+      } else {
+        request.account = twitterAccounts.last as! ACAccount
+
+        // 5 - perform the request
+        request.performRequestWithHandler({ (data, response, error) -> Void in
+          if (response.statusCode == 200) {
+
+            // 6 - on success, parse the response
+            let timelineData = try! NSJSONSerialization.JSONObjectWithData(data, options: .AllowFragments) as! NSDictionary
+            o.sendNext(timelineData)
+            o.sendCompleted()
+
+          } else {
+
+            // 7 - send an error on failure
+            o.sendFailed(invalidResponseError)
+          }
+        })
+      }
     }
   }
 }
